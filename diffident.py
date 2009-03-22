ac6e8ca9ebc4bdf28660a099fc7d606b12c77569
Andy Balaam <andybalaam@artificialworlds.net>
2009-03-22 22:20:14 +0000

    Initial commit - basic test for creating a side-by-side diff.

---
+++ diffident.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+def test_2_different():
+    file1 = [
+        "line 1 here",
+        "line 2 here",
+    ]
+
+    file2 = [
+        "line 1 here different",
+        "line 2 here different",
+    ]
+
+    diff = [
+        "--- test/file1.txt	2009-03-22 21:42:38.000000000 +0000",
+        "+++ test/file2.txt	2009-03-22 21:53:26.000000000 +0000",
+        "@@ -1,2 +1,2 @@",
+        "-line 1 here",
+        "-line 2 here",
+        "+line 1 here different",
+        "+line 2 here different",
+    ]
+
+    diffmodel = DiffModel( file1, file2, diff )
+
+    listview = ListView( diffmodel )
+
+    listview.set_columns( 80 )
+
+    assert( listview.get_string() ==
+"""line 1 here                           ++line 1 here different
+line 2 here                           ++line 2 here different
+""" )
+
+
+def test_all():
+    test_2_different()
+
+if __name__ == "__main__":
+    test_all()
+
+
