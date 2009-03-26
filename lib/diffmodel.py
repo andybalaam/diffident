2a15c12b5adbea7c954fb9bb190935086b284124
Andy Balaam <andybalaam@artificialworlds.net>
2009-03-26 16:27:31 +0000

    Implement diffmodel.get_lines in a sensible way that passes the current test.

--- lib/diffmodel.py
+++ lib/diffmodel.py
@@ -1,4 +1,13 @@
 
+def is_x_line( line, x ):
+	return ( line[0] == x and not line.startswith( x * 3 ) )
+
+def is_minus_line( line ):
+	return is_x_line( line, "-" )
+
+def is_plus_line( line ):
+	return is_x_line( line, "+" )
+
 class DiffModel:
 
 	class DiffLine:
@@ -12,9 +21,16 @@ class DiffModel:
 		self.diff = diff
 
 	def get_lines( self ):
-		return [
-			DiffModel.DiffLine( "line 1 here", "line 1 here different" ),
-			DiffModel.DiffLine( "line 2 here", "line 2 here different" ),
-		]
+		ret = []
+		left_lines = []
+
+		for line in self.diff:
+			if is_minus_line( line ):
+				left_lines.append( line[1:] )
+			elif is_plus_line( line ):
+				ret.append( DiffModel.DiffLine( left_lines[0], line[1:] ) )
+				left_lines = left_lines[1:]
+
+		return ret
 
 
