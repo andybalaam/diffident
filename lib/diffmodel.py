d35a969937d0655bc146951bb0573136d1c37b0d
Andy Balaam <andybalaam@artificialworlds.net>
2009-03-28 01:15:03 +0000

    First stab at parsing unified diffs.  Does not support deletions or additions. I.e. it only understands diffs which describe modifications to existing lines.

--- lib/diffmodel.py
+++ lib/diffmodel.py
@@ -1,3 +1,4 @@
+import re
 
 def is_x_line( line, x ):
 	return ( line[0] == x and not line.startswith( x * 3 ) )
@@ -20,29 +21,125 @@ class DiffModel:
 			self.left = left
 			self.right = right
 			self.status = status
+		def __repr__( self ):
+			return self.left + " -- " +  self.right
+
+	class CountingIter:
+		"""A wrapper around an iterator that keeps count of how many times
+		next() has been called"""
+
+		def __init__( self, iter_ ):
+			self.iter_ = iter_
+			self.num = 0
+
+		def __iter__( self ):
+			return self
+
+		def next( self ):
+			ans = self.iter_.next()
+			self.num += 1
+			return ans
+
+		def next_no_throw( self ):
+			try:
+				return self.next()
+			except StopIteration:
+				return None
 
 	def __init__( self, left_file, right_file, diff ):
 		self.left_file = left_file
 		self.right_file = right_file
 		self.diff = diff
 
-	def get_lines( self ):
-		ret = []
-		left_lines = []
+	hunk_line_re = re.compile( r"@@ -(\d+),\d+ \+\d+,\d+ @@" )
+	def parse_hunk_line( self, line ):
+		"""Examine the supplied line and return the first line of the left-hand
+		file if this is a hunk line, or -1 otherwise."""
 
-		for line in self.diff:
-			#if is_hunk_area( line ):
-			#
-			#el
-			if is_minus_line( line ):
-				left_lines.append( line[1:] )
-			elif is_plus_line( line ):
-				ret.append( DiffModel.DiffLine( left_lines[0], line[1:],
+		m = DiffModel.hunk_line_re.match( line )
+		if m:
+			return int( m.group( 1 ) )
+		else:
+			return -1
+
+	def read_left_to_linenum( self, ret, linenum, left_iter ):
+		while left_iter.num < linenum - 1:
+			try:
+				left_line = left_iter.next()
+			except StopIteration:
+				raise Exception(
+					"Reached the end of the left file unexpectedly." )
+			ret.append( DiffModel.DiffLine( left_line, left_line,
+				DiffModel.IDENTICAL ) )
+
+	def read_left_to_end( self, ret, left_iter ):
+		while True:
+			try:
+				left_line = left_iter.next()
+			except StopIteration:
+				break
+			ret.append( DiffModel.DiffLine( left_line, left_line,
+				DiffModel.IDENTICAL ) )
+
+	def read_diff_to_end_of_hunk( self, ret, left_iter, diff_iter ):
+		left_lines = []
+		hunk_left_begin = -1
+		while True:
+			# If this throws we allow the StopIteration exception to escape
+			# to our caller.
+			diff_line = diff_iter.next()
+
+			hunk_left_begin = self.parse_hunk_line( diff_line )
+			if hunk_left_begin > 0:
+				break
+
+			if is_minus_line( diff_line ):
+				left_lines.append( diff_line[1:] )
+				left_iter.next_no_throw()
+			elif is_plus_line( diff_line ):
+				ret.append( DiffModel.DiffLine( left_lines[0], diff_line[1:],
 					DiffModel.DIFFERENT ) )
 				left_lines = left_lines[1:]
+			else:
+				real_line = diff_line[1:]
+				ret.append( DiffModel.DiffLine( real_line, real_line,
+					DiffModel.IDENTICAL ) )
+				left_iter.next_no_throw()
 
-		for line in self.left_file[len(ret):]:
-			ret.append( DiffModel.DiffLine( line, line, DiffModel.IDENTICAL ) )
+		# This will fail when we consider diffs expressing deletions
+		assert( len( left_lines ) == 0 )
+
+		return hunk_left_begin
+
+	def get_lines( self ):
+		ret = []
+
+		diff_iter = self.diff.__iter__()
+		left_iter = DiffModel.CountingIter( self.left_file.__iter__() )
+
+		try:
+			diff_line = diff_iter.next()
+		except StopIteration:
+			diff_line = None
+
+		if diff_line is not None:
+			hunk_left_begin = self.parse_hunk_line( diff_line )
+			while True:
+				if hunk_left_begin > 0:
+					self.read_left_to_linenum( ret, hunk_left_begin, left_iter )
+					try:
+						hunk_left_begin = self.read_diff_to_end_of_hunk(
+							ret, left_iter, diff_iter )
+					except StopIteration:
+						break
+				else:
+					try:
+						diff_line = diff_iter.next()
+						hunk_left_begin = self.parse_hunk_line( diff_line )
+					except StopIteration:
+						break
+
+		self.read_left_to_end( ret, left_iter )
 
 		return ret
 
--- test/test_diffmodel.py
+++ test/test_diffmodel.py
@@ -1,6 +1,11 @@
 from lib.diffmodel import DiffModel
 from asserts import assert_strings_equal
 
+def parse_hunks():
+	diffmodel = DiffModel( None, None, None );
+	assert( diffmodel.parse_hunk_line( "@@ -1,2 +1,2 @@" ) == 1 )
+	assert( diffmodel.parse_hunk_line( "@@ -36,5 +36,5 @@" ) == 36 )
+
 def just_differences():
 	file1 = [
 		"line 1 here",
@@ -69,10 +74,10 @@ def diff_then_same():
 	]
 
 	file2 = [
-		"line 1 here",
-		"line 2 here",
-		"line 3 here different",
-		"line 4 here different",
+		"line 1 here different",
+		"line 2 here different",
+		"line 3 here",
+		"line 4 here",
 	]
 
 	diff = [
@@ -107,8 +112,116 @@ def diff_then_same():
 	assert_strings_equal( lines[3].right, "line 4 here" )
 	assert( lines[3].status == DiffModel.IDENTICAL )
 
+
+def same_then_diff():
+	file1 = [
+		"line 1 here",
+		"line 2 here",
+		"line 3 here",
+		"line 4 here",
+	]
+
+	file2 = [
+		"line 1 here",
+		"line 2 here",
+		"line 3 here different",
+		"line 4 here different",
+	]
+
+	diff = [
+		"--- test/file1.txt	2009-03-27 21:06:15.000000000 +0000",
+		"+++ test/file2.txt	2009-03-27 21:19:51.000000000 +0000",
+		"@@ -1,4 +1,4 @@",
+		" line 1 here",
+		" line 2 here",
+		"-line 3 here",
+		"-line 4 here",
+		"+line 3 here different",
+		"+line 4 here different",
+	]
+
+	diffmodel = DiffModel( file1, file2, diff )
+
+	lines = diffmodel.get_lines()
+
+	assert_strings_equal( lines[0].left,  "line 1 here" )
+	assert_strings_equal( lines[0].right, "line 1 here" )
+	assert( lines[0].status == DiffModel.IDENTICAL )
+
+	assert_strings_equal( lines[1].left,  "line 2 here" )
+	assert_strings_equal( lines[1].right, "line 2 here" )
+	assert( lines[1].status == DiffModel.IDENTICAL )
+
+	assert_strings_equal( lines[2].left,  "line 3 here" )
+	assert_strings_equal( lines[2].right, "line 3 here different" )
+	assert( lines[2].status == DiffModel.DIFFERENT )
+
+	assert_strings_equal( lines[3].left,  "line 4 here" )
+	assert_strings_equal( lines[3].right, "line 4 here different" )
+	assert( lines[3].status == DiffModel.DIFFERENT )
+
+def multiple_hunks_no_deletions_or_additions():
+	file1 = []
+	for i in range( 1, 41 ):
+		file1.append( "line %d here" % i )
+
+	file2 = []
+	for line in file1:
+		file2.append( line )
+
+	for i in ( 2, 39 ):
+		file2[i-1] += " different"
+
+	diff = [
+		"--- test/file1.txt	2009-03-28 00:34:14.000000000 +0000",
+		"+++ test/file2.txt	2009-03-28 00:34:33.000000000 +0000",
+		"@@ -1,5 +1,5 @@",
+		" line 1 here",
+		"-line 2 here",
+		"+line 2 here different",
+		" line 3 here",
+		" line 4 here",
+		" line 5 here",
+		"@@ -36,5 +36,5 @@",
+		" line 36 here",
+		" line 37 here",
+		" line 38 here",
+		"-line 39 here",
+		"+line 39 here different",
+		" line 40 here",
+	]
+
+	diffmodel = DiffModel( file1, file2, diff )
+
+	lines = diffmodel.get_lines()
+
+	assert_strings_equal( lines[0].left,  "line 1 here" )
+	assert_strings_equal( lines[0].right, "line 1 here" )
+	assert( lines[0].status == DiffModel.IDENTICAL )
+
+	assert_strings_equal( lines[1].left,  "line 2 here" )
+	assert_strings_equal( lines[1].right, "line 2 here different" )
+	assert( lines[1].status == DiffModel.DIFFERENT )
+
+	for i in range( 3, 39 ):
+		assert_strings_equal( lines[i-1].left,  "line %d here" % i )
+		assert_strings_equal( lines[i-1].right, "line %d here" % i )
+		assert( lines[i-1].status == DiffModel.IDENTICAL )
+
+	assert_strings_equal( lines[38].left,  "line 39 here" )
+	assert_strings_equal( lines[38].right, "line 39 here different" )
+	assert( lines[38].status == DiffModel.DIFFERENT )
+
+	assert_strings_equal( lines[39].left,  "line 40 here" )
+	assert_strings_equal( lines[39].right, "line 40 here" )
+	assert( lines[39].status == DiffModel.IDENTICAL )
+
 def run():
+	parse_hunks()
+
 	just_differences()
 	identical()
 	diff_then_same()
+	same_then_diff()
+	multiple_hunks_no_deletions_or_additions()
 
