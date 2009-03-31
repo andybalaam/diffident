76482a6a7a53b1cdfd96139d280780fd01f32ddd
Andy Balaam <andybalaam@artificialworlds.net>
2009-03-31 23:00:02 +0100

    Added a --test-slow option to run tests that touch the file system etc.

--- diffident.py
+++ diffident.py
@@ -2,13 +2,26 @@
 
 import sys
 
-from test.test_all import test_all
+from test.test_all import test_quick
+from test.test_all import test_slow
+
+from lib.main import diff_2_files
 
 def main():
-	if len( sys.argv ) > 1 and sys.argv[1] == "--test":
-		return test_all()
+	if len( sys.argv ) > 1:
+		if sys.argv[1] == "--test":
+			return test_quick()
+		elif sys.argv[1] == "--test-slow":
+			return test_slow()
 
-	print "Usage: ./diffident.py --test"
+	if len( sys.argv ) > 2:
+		return diff_2_files( sys.argv[1], sys.argv[2] )
+
+	sys.stderr.write( """Usage:
+./diffident.py file1 file2
+./diffident.py --test
+./diffident.py --test-slow
+""" )
 	sys.exit( 1 )
 
 if __name__ == "__main__":
--- test/test_all.py
+++ test/test_all.py
@@ -1,13 +1,24 @@
+import os
+
 import test_listview
 import test_diffmodel
 import test_unifieddiffparser
 
-def test_all():
+def run_quick_tests():
 	test_listview.run()
 	test_diffmodel.run()
 	test_unifieddiffparser.run()
 
+def test_quick():
+	run_quick_tests()
 	print "All tests passed."
-
 	return 0
+
+def test_slow():
+	run_quick_tests()
+
+	# TODO: don't use os.system, and handle return values correctly.
+	#retval = os.system( "test/slow/test_slow.sh" )
+	#if retval == 0:
+	print "All tests (including slow ones) passed."
 
