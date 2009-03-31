5acd2aaa273524e2eb6bd83925ac613a1fe40455
Andy Balaam <andybalaam@artificialworlds.net>
2009-03-31 20:55:00 +0100

    Comments and docstrings for UnifiedDiffParser.

--- lib/unifieddiffparser.py
+++ lib/unifieddiffparser.py
@@ -2,6 +2,8 @@ import re
 import lib.difflinetypes as difflinetypes
 
 def is_x_line( line, x ):
+	# TODO exclude only lines exactly matching the pattern for the beginning
+	# of a hunk.
 	return ( line[0] == x and not line.startswith( x * 3 ) )
 
 def is_minus_line( line ):
@@ -17,7 +19,7 @@ class UnifiedDiffParser:
 
 	class CountingIter:
 		"""A wrapper around an iterator that keeps count of how many times
-		next() has been called"""
+		next() has been called."""
 
 		def __init__( self, iter_ ):
 			self.iter_ = iter_
@@ -32,6 +34,8 @@ class UnifiedDiffParser:
 			return ans
 
 		def next_no_throw( self ):
+			"""Call next and swallow any StopIteration exception.  If such
+			an exception was thrown, return None."""
 			try:
 				return self.next()
 			except StopIteration:
@@ -54,6 +58,9 @@ class UnifiedDiffParser:
 			return -1
 
 	def read_left_to_linenum( self, line_callback, linenum, left_iter ):
+		"""Read from the left-hand file, calling back to say each line
+		is identical, until we reach the specified line number."""
+
 		while left_iter.num < linenum - 1:
 			try:
 				left_line = left_iter.next()
@@ -63,6 +70,9 @@ class UnifiedDiffParser:
 			line_callback( left_line, left_line, difflinetypes.IDENTICAL )
 
 	def read_left_to_end( self, line_callback, left_iter ):
+		"""Read all the way to the end of the left file, calling back to
+		say each line is identical."""
+
 		while True:
 			try:
 				left_line = left_iter.next()
@@ -71,27 +81,43 @@ class UnifiedDiffParser:
 			line_callback( left_line, left_line, difflinetypes.IDENTICAL )
 
 	def process_removed_lines( self, line_callback, left_lines ):
+		"""Call back for each line in left_lines saying it was removed."""
+
 		for left_line in left_lines:
 			line_callback( left_line, None, difflinetypes.REMOVE )
 		# Clear the list
 		left_lines[:] = []
 
 	def read_diff_to_end_of_hunk( self, line_callback, left_iter, diff_iter ):
+		"""Understand a hunk of Unified Diff Format, calling back for
+		identical, changed, added and removed lines as specified."""
+
 		left_lines = []
 		hunk_left_begin = -1
 		while True:
-			# If this throws we allow the StopIteration exception to escape
-			# to our caller.
 			try:
 				diff_line = diff_iter.next()
 			except StopIteration, e:
+				# This means we have reached the end of the diff.  Anything
+				# that is left in left_lines must have been removed.
 				self.process_removed_lines( line_callback, left_lines )
+				# We re-raise the exception to let our caller know we have
+				# finished reading the diff.
 				raise e
 
+			# Find out whether this line is the beginning of hunk
 			hunk_left_begin = self.parse_hunk_line( diff_line )
+			# If it is, we must stop because we have finsished with the
+			# previous hunk
 			if hunk_left_begin > 0:
 				break
 
+			# Look for each type of line.  We maintain a list of lines
+			# beginning with '-', called left_lines.  When we hit a '+'
+			# we can cancel out a minus and make a DIFFERENT line, or if
+			# there are no minuses this is an ADD line.
+			# If we are left with any minuses we make them REMOVE lines.
+			# Lines starting with ' ' are IDENTICAL lines.
 			if is_minus_line( diff_line ):
 				left_lines.append( diff_line[1:] )
 				left_iter.next_no_throw()
@@ -110,6 +136,10 @@ class UnifiedDiffParser:
 					difflinetypes.IDENTICAL )
 				left_iter.next_no_throw()
 
+		# This tells our caller that we found a new hunk line (we must have
+		# done since the only alternative is that we hit the end of the diff
+		# in which case we raised an exception), and what line number it said
+		# the new hunk starts at (in the left file).
 		return hunk_left_begin
 
 	def parse_lines( self, line_callback ):
@@ -122,6 +152,11 @@ class UnifiedDiffParser:
 		diff_iter = self.diff.__iter__()
 		left_iter = UnifiedDiffParser.CountingIter( self.left_file.__iter__() )
 
+		# Go through all lines in the diff, and if we find the beginning of
+		# hunk, read from the left file up to that hunk (since the files are
+		# identical where there is no hunk), then process the hunk, then
+		# repeat.  At the end, read to the end of the left file.
+
 		try:
 			diff_line = diff_iter.next()
 		except StopIteration:
@@ -130,6 +165,7 @@ class UnifiedDiffParser:
 		if diff_line is not None:
 			hunk_left_begin = self.parse_hunk_line( diff_line )
 			while True:
+				# If this line was beginning a hunk, read the hunk
 				if hunk_left_begin > 0:
 					self.read_left_to_linenum( line_callback, hunk_left_begin, left_iter )
 					try:
@@ -138,6 +174,8 @@ class UnifiedDiffParser:
 					except StopIteration:
 						break
 				else:
+					# Otherwise, go the next line and find out whether it is
+					# the beginning of a hunk
 					try:
 						diff_line = diff_iter.next()
 						hunk_left_begin = self.parse_hunk_line( diff_line )
