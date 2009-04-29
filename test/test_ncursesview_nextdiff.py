b1cf3ee4c328adaa054654dc5b5fc69d5c3a1277
Andy Balaam <andybalaam@artificialworlds.net>
2009-04-29 23:09:47 +0100

    Added a test that goes to the next diff twice, and moves right too.

--- test/test_ncursesview_nextdiff.py
+++ test/test_ncursesview_nextdiff.py
@@ -8,7 +8,7 @@ from testlib.fakediffmodel import FakeDiffModel
 import lib.difflinetypes as difflinetypes
 from lib.ncursesview import NCursesView
 
-def next_diff_different():
+def _make_view():
 	diffmodel = FakeDiffModel()
 	diffmodel.lines = [
 		FakeDiffLine( "line 01", "line 01", difflinetypes.IDENTICAL ),
@@ -21,16 +21,22 @@ def next_diff_different():
 		FakeDiffLine( "line 08", "line 08", difflinetypes.IDENTICAL ),
 		FakeDiffLine( "line 09", "line 09", difflinetypes.IDENTICAL ),
 		FakeDiffLine( "line 10", "line 10 different", difflinetypes.DIFFERENT ),
-		FakeDiffLine( "line 11", "line 11", difflinetypes.IDENTICAL ),
+		FakeDiffLine( "line 11", "line 11 different", difflinetypes.DIFFERENT ),
 		FakeDiffLine( "line 12", "line 12", difflinetypes.IDENTICAL ),
 		FakeDiffLine( "line 13", "line 13", difflinetypes.IDENTICAL ),
-		FakeDiffLine( "line 14", "line 14", difflinetypes.IDENTICAL ),
+		FakeDiffLine( None,      "line 14", difflinetypes.ADD ),
 		FakeDiffLine( "line 15", "line 15", difflinetypes.IDENTICAL ),
 	]
 
 	view = NCursesView( diffmodel )
 	view.win_width = 20
 	view.win_height = 7
+
+	return view
+
+def next_diff_different():
+
+	view = _make_view()
 
 	actions = [ "n" ]
 
@@ -39,12 +45,31 @@ def next_diff_different():
 line 08    line 08  
 line 09    line 09  
 [di]line 10 [n] * [d]line 10 d
-[n]line 11    line 11  
-line 12    line 12  
+line 11 [n] * [d]line 11 d
+[n]line 12    line 12  
 line 13    line 13  
 """ )
+
+def next_diff_several():
+
+	view = _make_view()
+
+	actions = [ "l", "n", "n" ]
+
+	assert_strings_equal( view.show( actions ),
+"""[d]line 11 [n] * [d]line 11 d
+[n]line 12    line 12  
+line 13    line 13  
+[m]........[n] + [ai]line 14  
+[n]line 15    line 15  
+                    
+                    
+""" )
+
+
 
 
 def run():
 	next_diff_different()
+	next_diff_several()
 
